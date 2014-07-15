9e0a66ecf98236de917b7f95123ffb6502d1869d
marcoaam <marco.army@gmail.com>
2014-07-15 09:36:53 +0100

    Horizontal and vertical methods added

--- lib/board.rb
+++ lib/board.rb
@@ -1,38 +1,34 @@
 class Board
 
-	attr_accessor :grid_hash
+	attr_accessor :grid_hash, :tracking, :personal
 
-	def initialize
-		@personal = create_grid
-		@tracking = create_grid	
-	end
-
-	def personal
-		@personal
-	end
-
-	def tracking
-		@tracking
+	def initialize(x=10, y=10)
+		@personal = create_grid(x,y)
+		@tracking = create_grid(x,y)
 	end
 
 	def create_grid(x=10,y=10)
 		@grid_hash = {}
-  	(1..x).each do |a|
-    	(1..y).each do |b|
-      @grid_hash["#{(a+64).chr}#{b}".to_sym] = nil
+  	(1..x).each do |letter|
+    	(1..y).each do |number|
+      @grid_hash["#{(letter+64).chr}#{number}".to_sym] = nil
     	end
   	end
 	end
+
+	def coordinates_to_take(ship, coordinates, orientation='h')
+		return horizontal(ship, coordinates) if orientation == 'h'
+		vertical(ship, coordinates)
+	end
+
+	def horizontal(ship, coordinates)
+		coordinate_letter, coordinate_number = coordinates.chars.first, coordinates.chars.last
+		(0...ship.length).map { |number| (coordinate_letter.bytes.first + number).chr + coordinate_number }
+	end
+
+	def vertical(ship, coordinates)
+		coordinate_letter, coordinate_number = coordinates.chars.first, coordinates.chars.last.to_i
+		(0...ship.length).map { |number| coordinate_letter + (coordinate_number + number).to_s }
+	end
+	
 end
-
-
-# Hash creator
-
-# def initialize(x=1,y=1)
-#   @hash = {}
-#   (1..x).each do |a|
-#     (1..y).each do |b|
-#       @hash["#{(a+64).chr}#{b}".to_sym] = nil
-#     end
-#   end
-# end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,7 +8,6 @@ describe Board do
   	board = Board.new
   	board.create_grid(2,2)
   	expect(board.grid_hash).to eq ({A1: nil, A2: nil, B1: nil, B2: nil})
-
   end
 
   it 'it has a personal board created with a default size of 10 x 10' do
@@ -19,6 +18,18 @@ describe Board do
   it 'it has a tracking board that looks the same as the personal board' do
   	board = Board.new
   	expect(board.tracking).to eq (board.create_grid)
+  end
+
+  it 'it can determine the horizontal coordinates providing a ship length and start coordinate' do
+    ship = double :shsip, length: 2
+    coorinates = "B1"
+    expect(board.horizontal(ship, coorinates)).to eq ["B1", "C1"]
+  end
+
+  it 'it can determine the vertical coordinates providing a ship length and start coordinate' do
+    ship = double :ship, length: 2
+    coorinates = "A2"
+    expect(board.vertical(ship, coorinates)).to eq ["A2", "A3"]
   end
 
 end
