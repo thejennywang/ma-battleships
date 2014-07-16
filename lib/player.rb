757a7a385922b2e353ea183f55c92b9118325b9e
marcoaam <marco.army@gmail.com>
2014-07-16 17:58:39 +0100

    player version progress

--- lib/player.rb
+++ lib/player.rb
@@ -1,12 +1,25 @@
+require './lib/board'
+require './lib/ship'
+
 class Player
 
-	attr_reader :name
+	attr_reader :name, :board
+	attr_accessor :ships
 
 	def initialize(option = {})
+		@ships = [carrier = Ship.carrier, battleship= Ship.battleship, submarine= Ship.submarine, patrol= Ship.patrol]
+		@board = Board.new
 		@name = option.fetch(:name, "player1")
 	end
 
-	def call_shot(coordinate)
-		coordinate
+	def shoot_at(player, coordinate)
+		player.board.attacked_at(coordinate) 
 	end
+
+	def place(coordinate, orientation)
+		ship = @ships.pop
+		board.place(ship, coordinate, orientation)
+	end
+
 end
+
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,17 +2,48 @@ require 'player'
 
 describe Player do
 
+let (:player1) {Player.new(name: "Michiel")}
 
 	it 'has a name' do
-		player1 = Player.new(name: "Michiel")
 		expect(player1.name).to eq "Michiel"
 	end
 
-	it 'can call shots' do 
-		coordinate = "A2"
-		player1 = Player.new
-		expect(player1.call_shot(coordinate)).to eq "A2"
+	it 'expect the player to have a board' do
+		expect(player1.board).to be_an_instance_of Board
 	end
 
+	it 'expect the player to start with 4 ships' do
+		expect(player1.ships.count).to eq 4
+	end
 
+	it 'expect the first ship to be an instance of the Ship class' do
+		expect(player1.ships.first).to be_an_instance_of Ship
+	end
+
+	it 'expect the first ship to be a carrier (i.e length = 5)' do
+		expect(player1.ships.first.length).to eq 5
+	end
+
+	it 'can shoot at each other' do 
+		coordinate = "A2"
+		marco_attacking = Player.new
+		chloe_receiving = Player.new
+		expect(chloe_receiving.board).to receive(:attacked_at).with(coordinate)
+		marco_attacking.shoot_at(chloe_receiving,coordinate)
+	end
+
+	it 'passes the ship and coordinates to the board so the ship to be placed' do
+		coordinate = "A2"
+		orientation = 'horizontal'
+		expect(player1.board).to receive(:place)
+		player1.place(coordinate,orientation)
+	end
+
+	it 'once a ship is placed the player no longer has it' do
+		coordinate = "A2"
+		orientation = 'horizontal'
+		allow(player1.board).to receive(:place)
+		player1.place(coordinate,orientation)
+		expect(player1.ships.count).to eq 3
+	end
 end
