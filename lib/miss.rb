134f17988f1ffafeaf76e7b947b613027ad2e53c
Michiel Stigter <michielstigter@hotmail.com>
2014-07-16 11:40:23 +0100

    Chloe & Michiel | Added water and miss class

---
+++ lib/miss.rb
@@ -0,0 +1,14 @@
+class Miss
+
+	def initialize
+		more_initialize('param')
+	end
+
+	def more_initialize(param)
+		@something = param
+	end
+
+	attr_reader :something
+git 
+
+end
---
+++ spec/water_spec.rb
@@ -0,0 +1,10 @@
+require 'water'
+
+describe Water do 
+
+	it 'water can be attacked' do
+	 	water = Water.new
+	 	expect(water.attack!).to be_an_instance_of Miss
+
+	end
+end
