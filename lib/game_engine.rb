4d7946e197cc17cc91ce3824f577f4c1c5ee2146
marcoaam <marco.army@gmail.com>
2014-07-17 17:26:02 +0100

    changes to game engine

---
+++ lib/game_engine.rb
@@ -0,0 +1,75 @@
+require_relative 'board'
+require_relative 'player'
+require_relative 'cell'
+require_relative 'water'
+require_relative 'miss'
+require_relative 'hit'
+
+class Game
+
+	attr_reader :player1, :player2
+	attr_accessor :coordinate, :orientation
+
+	def get_user_input
+		STDIN.gets.chomp
+	end
+
+	def get_coordinates
+		puts("Please enter the coordinate")
+		@coordinate = get_user_input
+		coordinate
+	end
+
+	def get_orientation
+		puts("Please enter the orientation as 'horizontal' or 'vertical'")
+		@orientation = get_user_input
+		orientation
+	end
+
+	def get_ship_name
+		puts("Please enter the ship you want to place")
+		get_user_input
+	end
+
+	def get_name
+		puts("Please enter player name")
+		get_user_input
+	end
+
+	def new_board
+		Board.new
+	end
+
+	def set_of_ships
+		carrier, battleship, submarine, patrol  =  Ship.carrier, Ship.battleship, Ship.submarine, Ship.patrol
+		{"carrier" => carrier, "battleship" => battleship, "submarine" => submarine, "patrol" => patrol}
+	end
+
+	def new_player
+		Player.new(get_name, new_board, set_of_ships)
+	end
+
+	def create_match
+		@player1 = new_player
+		select_ship_to_be_placed(player1)
+		@player2 = new_player
+		select_ship_to_be_placed(player2)
+	end
+
+	# def place_ships(player)
+	# 	player.place(select_ship_to_be_placed(player), coordinate, orientation)
+	# end
+
+	def select_ship_to_be_placed(player)
+		puts "Please #{player.name} select a ship from your remaining ships"
+		puts "The remaining ships are:"
+		puts "#{player.ships.keys}"
+		player.place(delete_ship_from(player, get_ship_name), get_coordinates, get_orientation)
+	end
+
+	def delete_ship_from(player, ship_name)
+		player.ships.delete(ship_name)
+	end
+
+
+end
---
+++ spec/game_engine_spec.rb
@@ -0,0 +1,73 @@
+require 'game_engine'
+
+describe Game do
+
+	let(:game) { Game.new }
+
+	it 'promts for the player names when created' do
+		expect(STDIN).to receive(:gets).and_return("Marco")
+		expect(game.get_user_input).to eq "Marco"
+	end
+
+	it 'promts for the player names when created' do
+		expect(game).to receive(:get_user_input)
+		game.get_name
+	end
+
+	it 'can create boards' do
+		expect(game.new_board).to be_a Board
+	end
+
+	it 'can a create a set of new ships to be pased to each player' do
+		expect(game.set_of_ships["carrier"]).to be_a Ship
+	end
+
+	it 'can create players' do 
+		allow(game).to receive(:get_name).and_return("Marco")
+		expect(game.new_player).to be_a Player
+	end
+
+	it 'can create a match with 2 players' do
+		allow(game).to receive(:get_name).and_return("Marco").at_least(:twice)
+		allow(game).to receive(:get_ship_name).and_return("carrier").at_least(:twice)
+		allow(game).to receive(:get_coordinates).and_return("A1").at_least(:twice)
+		allow(game).to receive(:get_orientation).and_return("horizontal").at_least(:twice)
+		game.create_match
+		expect(game.player1).to be_a Player
+		expect(game.player2).to be_a Player
+	end
+
+	it 'promts for the player to enter coordinates' do
+		expect(game).to receive(:get_user_input).and_return("A1")
+		game.get_coordinates
+	end
+
+	it 'promts for the player to enter orientation' do
+		expect(game).to receive(:get_user_input).and_return("vertical")
+		game.get_orientation
+	end
+
+	it 'lets the player select a ship from his ships' do
+		carrier = double :carrier
+		player1 = double :player, name: "marco", ships: {"carrier" => carrier}, place: carrier
+		allow(game).to receive(:get_ship_name).and_return("carrier")
+		allow(game).to receive(:get_coordinates).and_return("A1")
+		allow(game).to receive(:get_orientation).and_return("horizontal")
+
+		expect(game.select_ship_to_be_placed(player1)).to eq carrier
+	end
+
+	it 'promts for the player to enter ship name' do
+		expect(game).to receive(:get_user_input).and_return("carrier")
+		game.get_ship_name
+	end
+
+	it 'selects and delete a ship' do 
+		ships = double(:ships)
+		player = double(:player, ships: ships)
+
+		expect(ships).to receive(:delete).with("carrier")
+		game.delete_ship_from(player, "carrier")
+	end
+
+end
