fe74de4c594d7d174eeda440f25a78205f22cf03
marcoaam <marco.army@gmail.com>
2014-07-17 10:17:33 +0100

    board changed

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,29 +7,24 @@ describe Board do
   it 'Creates a new grid empty by default' do
     board = Board.new
     board.create_grid(1,1)
-    expect(board.grid_hash["A1"]).to be_an_instance_of Cell
+    expect(board.grid["A1"]).to be_an_instance_of Cell
   end
 
-  it 'it has a personal board created with a default size of 10 x 10' do
+  it 'Creates a new grid empty by default' do
     board = Board.new
-    expect(board.personal).to eq (board.create_grid)
+    expect(board.grid).to eq board.grid
   end
 
-  it 'it has a tracking board that looks the same as the personal board' do
-    board = Board.new
-    expect(board.tracking).to eq (board.create_grid)
-  end
-
-  it 'get the coodinates that the ship take' do
+  it 'get the coodinates that the ship take horizontaly' do
     ship = double :ship, length: 2
     coordinate = "A1"
     expect(board.get_coordinates_for(ship, starting_on: coordinate, running: "horizontal")).to eq ["A1", "B1"]
   end
 
-  it 'get the coodinates that the ship take' do
+   it 'get the coodinates that the ship take verticaly' do
     ship = double :ship, length: 2
     coordinate = "A1"
-    expect(board.get_coordinates_for(ship, starting_on: coordinate, running: "horizontal")).to eq ["A1", "B1"]
+    expect(board.get_coordinates_for(ship, starting_on: coordinate, running: "vertical")).to eq ["A1", "A2"]
   end
 
   it 'it can determine the horizontal coordinates providing a ship length and start coordinate' do
@@ -49,8 +44,14 @@ describe Board do
     coordinates = "A1"
     running = 'horizontal'
     board.place(ship, coordinates, running)
-    expect(board.grid_hash["A1"].content).to be ship
-    expect(board.grid_hash["B1"].content).to be ship
+    expect(board.grid["A1"].content).to be ship
+    expect(board.grid["B1"].content).to be ship
+  end
+
+  it 'can record an attack into the grid cells' do
+    coordinate = "A1"
+    board.attacked_at(coordinate)
+    expect(board.grid[coordinate].content).to be_an_instance_of Miss
   end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,26 +2,26 @@ require 'player'
 
 describe Player do
 
-let (:player1) {Player.new(name: "Michiel")}
+let (:player) {Player.new(name: "Michiel")}
 
 	it 'has a name' do
-		expect(player1.name).to eq "Michiel"
+		expect(player.name).to eq "Michiel"
 	end
 
 	it 'expect the player to have a board' do
-		expect(player1.board).to be_an_instance_of Board
+		expect(player.board).to be_an_instance_of Board
 	end
 
 	it 'expect the player to start with 4 ships' do
-		expect(player1.ships.count).to eq 4
+		expect(player.ships.count).to eq 4
 	end
 
 	it 'expect the first ship to be an instance of the Ship class' do
-		expect(player1.ships.first).to be_an_instance_of Ship
+		expect(player.ships.first).to be_an_instance_of Ship
 	end
 
 	it 'expect the first ship to be a carrier (i.e length = 5)' do
-		expect(player1.ships.first.length).to eq 5
+		expect(player.ships.first.length).to eq 5
 	end
 
 	it 'can shoot at each other' do 
@@ -35,15 +35,15 @@ let (:player1) {Player.new(name: "Michiel")}
 	it 'passes the ship and coordinates to the board so the ship to be placed' do
 		coordinate = "A2"
 		orientation = 'horizontal'
-		expect(player1.board).to receive(:place)
-		player1.place(coordinate,orientation)
+		expect(player.board).to receive(:place)
+		player.place(coordinate,orientation)
 	end
 
 	it 'once a ship is placed the player no longer has it' do
 		coordinate = "A2"
 		orientation = 'horizontal'
-		allow(player1.board).to receive(:place)
-		player1.place(coordinate,orientation)
-		expect(player1.ships.count).to eq 3
+		allow(player.board).to receive(:place)
+		player.place(coordinate,orientation)
+		expect(player.ships.count).to eq 3
 	end
 end
